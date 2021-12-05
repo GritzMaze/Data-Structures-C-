dfaf1dce1df0aea1f7d4baa7b2d0827da2c6d551
GritzMaze <jivkovvesselin@gmail.com>
2021-12-05 20:42:58 +0200

    Added some tests in main

--- HomeWork-1/main.cpp
+++ HomeWork-1/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include "queue.h"
+#include "myStore.h"
 
 int main(int, char**) {
-    Queue temp;
-    temp.enqueue(1);
-    temp.enqueue(2);
-    temp.enqueue(3);
+    Queue<int> temp;
+    temp.enqueue(1, 14);
+    temp.enqueue(2, 3);
+    temp.enqueue(3, 25);
 
     std::cout << temp.dequeue() << "\n";
     std::cout << temp.dequeue() << "\n";
     std::cout << temp.dequeue() << "\n";
 
     std::cout << "Hello, world!\n";
+
+
+    MyStore store;
+    Client client1{1,1,1,1};
+    Client client2{2,2,2,2};
+
+    store.init(1, 1, 1);
+    store.addClients(&client1, 1);
+    store.addClients(&client2, 1);
+
 }
