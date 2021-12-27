fa1f22eaa8e388179b7ebe609c479f0dd83db393
GritzMaze <jivkovvesselin@gmail.com>
2021-12-27 23:46:11 +0200

    Added tree implementation

--- HomeWork-2/main.cpp
+++ HomeWork-2/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "Tree.h"
+#include <string>
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
+
+    Tree<std::string> tree;
+
     return 0;
 }
---
+++ HomeWork-2/tree.cpp
@@ -0,0 +1,229 @@
+#include "tree.h"
+
+template <class DataType>
+Tree<DataType>::Tree(const Tree &other)
+{
+    root = copy(other.root);
+    size = other.size;
+}
+
+template <class DataType>
+Tree<DataType>::Tree(Tree &&other) noexcept
+{
+    root = other.root;
+    size = other.size;
+    other.root = nullptr;
+    other.size = 0;
+}
+
+template <class DataType>
+Tree<DataType>::~Tree()
+{
+    clear(root);
+}
+
+template <class DataType>
+Tree<DataType> &Tree<DataType>::operator=(const Tree &other)
+{
+    if (this != &other)
+    {
+        clear(root);
+        root = copy(other.root);
+        size = other.size;
+    }
+    return *this;
+}
+
+template <class DataType>
+Tree<DataType> &Tree<DataType>::operator=(Tree &&other) noexcept
+{
+    if (this != &other)
+    {
+        clear(root);
+        root = other.root;
+        size = other.size;
+        other.root = nullptr;
+        other.size = 0;
+    }
+    return *this;
+}
+
+template <class DataType>
+bool Tree<DataType>::find(const DataType &data) const
+{
+    return find(data, root);
+}
+
+template <class DataType>
+bool Tree<DataType>::remove(const DataType &data)
+{
+    return remove(data, root);
+}
+
+template <class DataType>
+int Tree<DataType>::getHeight() const
+{
+    return height(root);
+}
+
+template <class DataType>
+int Tree<DataType>::getSize() const
+{
+    return size;
+}
+
+template <class DataType>
+bool Tree<DataType>::isEmpty() const
+{
+    return size == 0;
+}
+
+template <class DataType>
+Node<DataType> *Tree<DataType>::copy(Node<DataType> *other)
+{
+    if (other == nullptr)
+    {
+        return nullptr;
+    }
+    Node *newNode = new Node(other->data, copy(other->child), copy(other->siblings));
+    return newNode;
+}
+
+template <class DataType>
+void Tree<DataType>::clear(Node<DataType> *node)
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+    clear(node->child);
+    clear(node->siblings);
+    delete node;
+}
+
+template <class DataType>
+bool Tree<DataType>::find(const DataType &data, Node<DataType> *node) const
+{
+    if (node == nullptr)
+    {
+        return false;
+    }
+    if (node->data == data)
+    {
+        return true;
+    }
+    return find(data, node->child) || find(data, node->siblings);
+}
+
+template <class DataType>
+bool Tree<DataType>::remove(const DataType &data, Node<DataType> *&root)
+{
+    if (root == nullptr)
+    {
+        return false;
+    }
+
+    // If data is found
+    if (root->data == data)
+    {
+        Node<DataType> *temp = root;
+        // If root has no children
+        if (root->child == nullptr)
+        {
+            root = root->siblings;
+        }
+        else
+        { // If root has children
+            Node<DataType> *child = root->child;
+            while (child->siblings != nullptr)
+            { // Find the last child
+                child = child->siblings;
+            }
+            child->siblings = root->siblings; // Set the last child's sibling to root's sibling
+            root = root->child;               // Up a level
+        }
+        delete temp;
+        size--;
+        return true;
+    }
+    return remove(data, root->child) || remove(data, root->siblings);
+}
+
+template <class DataType>
+int Tree<DataType>::height(const Node<DataType> *node) const
+{
+    if (node == nullptr)
+    {
+        return 0;
+    }
+    return std::max(1 + height(node->child), height(node->siblings));
+}
+
+template <class DataType>
+void Tree<DataType>::insert(const DataType &data, const string &name)
+{
+    return insert(data, name, root);
+}
+
+template <class DataType>
+bool Tree<DataType>::insert(const DataType &data, const string &name, Node<DataType> *&root)
+{
+    if (root == nullptr)
+    {
+        return false;
+    }
+
+    if (root->data == name)
+    {
+        if (root->child == nullptr)
+        {
+            root->child = new Node<DataType>(data);
+            size++;
+            return true;
+        }
+        else
+        {
+            Node<DataType> *child = root->child;
+            while (child->siblings != nullptr)
+            {
+                child = child->siblings;
+            }
+            child->siblings = new Node<DataType>(data);
+        }
+    }
+    else
+    {
+        return insert(data, name, root->child) || insert(data, name, root->siblings);
+    }
+    return false;
+}
+
+template <class DataType>
+void Tree<DataType>::printByLevels(const Node<DataType> *root) const
+{
+    if (!root)
+        return;
+    std::queue<const Node<DataType> *> front;
+    front.push(root);
+    front.push(nullptr);
+    for (;;)
+    {
+        const node *current = front.front();
+        front.pop();
+        if (!current)
+        {
+            std::cout << '\n';
+            if (front.empty())
+                return;
+            front.push(nullptr);
+        }
+        else
+        {
+            std::cout << current->data << ' ';
+            for (const Node<DataType> *it = current->child; it; it = it->siblings)
+            {
+                front.push(it);
+            }
+        }
+    }
+}
